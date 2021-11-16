abfea20bf57a2d1498b1a05f57c1b7f6617b9e72
ShaGrib <81344044+ShaGrib@users.noreply.github.com>
2021-11-16 18:26:11 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeLog() {
+    var x = document.getElementById("log").innerText="Logout";
+}
+
+function remAddBtn(el) {
+    var element = el;
+    element.remove();
+}
+
+function liked() {
+    alert("Ninja was liked");
+}
